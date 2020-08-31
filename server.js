46fead260c055f096fe1bb009c3346eec02809d9
Jean <nguyen.jean@me.com>
2020-08-31 17:18:27 +0200

    add api/migrations/artists files

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const morgan = require('morgan');
 const apiRouter = require('./api/api');
 
+const app = express();
+
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-app.use(errorhandler());
 app.use(cors());
+app.use(morgan('dev'));
+
+// routes
 app.use('/api', apiRouter);
+
+// handling error
+app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log(`Server is ready at http://localhost:${PORT}`);
