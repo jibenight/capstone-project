62c640f9cc1155cf652a5cfb0117cafe9f9578bc
Jean <nguyen.jean@me.com>
2020-09-07 21:43:37 +0200

    add put series

--- api/api.js
+++ api/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const artistsRouter = require('./artists.js');
-
 apiRouter.use('/artists', artistsRouter);
+
+const seriesRouter = require('./series.js');
+apiRouter.use('/series', seriesRouter);
 
 module.exports = apiRouter;
